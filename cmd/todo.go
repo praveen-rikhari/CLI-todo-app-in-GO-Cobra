337b1cff581eff578f794932053f7d970eac69e7
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-04 16:59:56 +0530

    Saving todos in a JSON file

--- cmd/todo.go
+++ cmd/todo.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -14,6 +16,49 @@ type TodoItem struct {
 }
 
 var todoList []TodoItem
+
+// File path for storing todos
+const todoFilePath = "todos.json"
+
+// Function to save todos to a JSON file
+func saveTodosToFile(todos []TodoItem) error {
+	// Marshal todos slice to JSON
+	data, err := json.MarshalIndent(todos, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	// Write JSON data to file
+	err = ioutil.WriteFile(todoFilePath, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Function to load todos from a JSON file
+func loadTodosFromFile() ([]TodoItem, error) {
+	var todos []TodoItem
+
+	// Read the JSON file
+	file, err := ioutil.ReadFile(todoFilePath)
+	if err != nil {
+		// Return empty slice if file doesn't exist
+		if os.IsNotExist(err) {
+			return todos, nil
+		}
+		return nil, err
+	}
+
+	// Unmarshal JSON data into todos slice
+	err = json.Unmarshal(file, &todos)
+	if err != nil {
+		return nil, err
+	}
+
+	return todos, nil
+}
 
 var todoCmd = &cobra.Command{
 	Use:   "todo",
@@ -60,6 +105,13 @@ func createTodo() {
 		}
 		todoList = append(todoList, newTodo)
 
+		// Save todos to file
+		err := saveTodosToFile(todoList)
+		if err != nil {
+			fmt.Println("Error saving todos:", err)
+			return
+		}
+
 		fmt.Printf("Todo created with ID: %d, Task: %s\n", newTodo.ID, newTodo.Task)
 	} else {
 		fmt.Println("Error reading input:", scanner.Err())
@@ -68,12 +120,21 @@ func createTodo() {
 
 // fucntion for listing out all the todos
 func listTodos() {
+
+	// Load todos from file
+	todos, err := loadTodosFromFile()
+	if err != nil {
+		fmt.Println("Error loading todos:", err)
+		return
+	}
+
 	fmt.Println("Todo List 📄:")
-	if len(todoList) == 0 {
+	if len(todos) == 0 {
 		fmt.Println("Currently no todos.Please make one.")
 		return
 	}
-	for _, todo := range todoList {
+
+	for _, todo := range todos {
 		fmt.Printf("ID : %d | Task : %s\n", todo.ID, todo.Task)
 	}
 }
