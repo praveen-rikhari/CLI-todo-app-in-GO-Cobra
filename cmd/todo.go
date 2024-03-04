566ce4b11b9887ad17c9d2adf28693f1cdd7b027
praveen-rikhari <praveenrikhari45@gmail.com>
2024-03-04 16:19:18 +0530

    Created todo commnad for creation of todo

--- cmd/todo.go
+++ cmd/todo.go
@@ -1,10 +1,19 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
+
+type TodoItem struct {
+	ID   int    `json:"id"`
+	Task string `json:"task"`
+}
+
+var todoList []TodoItem
 
 var todoCmd = &cobra.Command{
 	Use:   "todo",
@@ -17,6 +26,38 @@ var todoCmd = &cobra.Command{
 	},
 }
 
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new Todo",
+	Run: func(cmd *cobra.Command, args []string) {
+		createTodo()
+	},
+}
+
+func createTodo() {
+	// Prompt the user to enter the task for the todo item
+	fmt.Print("Enter your task : ")
+
+	// Create a scanner to read user input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Scan the input until newline character
+	if scanner.Scan() {
+		task := scanner.Text()
+
+		newTodo := TodoItem{
+			ID:   len(todoList) + 1,
+			Task: task,
+		}
+		todoList = append(todoList, newTodo)
+
+		fmt.Printf("Todo created with ID: %d, Task: %s\n", newTodo.ID, newTodo.Task)
+	} else {
+		fmt.Println("Error reading input:", scanner.Err())
+	}
+}
+
 func init() {
 	rootCmd.AddCommand(todoCmd)
+	todoCmd.AddCommand(createCmd)
 }
